9432bcffb33e10a55a26adfea8f76d9c5f501f59
ashacallaway <luckiedeka@gmail.com>
2022-10-19 19:12:00 -0400

    fixes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-import App from './app.json';
-
 const path = require('path');
 
 var port = "3000"
@@ -9,17 +7,17 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is up on port ${port}!`);
+// });
 
-var app1= App.new({
-  name: "small-sharp-tool",
-  description: "This app does one little thing, and does it well."
-})
+// var app1= app.new({
+//   name: "small-sharp-tool",
+//   description: "This app does one little thing, and does it well."
+// })
 
-App.fetch("github:zeke/slideshow", function(err, manifest) {
-  console.log(err, manifest)
-})
+// app1.fetch("github:zeke/slideshow", function(err, manifest) {
+//   console.log(err, manifest)
+// // })
 
-App.example
+// app1.example
